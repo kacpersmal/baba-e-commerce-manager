e09037acab6d3a031758d7adc99ed674ac03af4f
Kacper S <kacpersmal@gmail.com>
2025-10-25 23:19:55 +0200

    Update index.ts

--- src/client-app/src/lib/api/index.ts
+++ src/client-app/src/lib/api/index.ts
@@ -1,8 +1,3 @@
 export { apiClient } from './client'
-export {
-  useHealthCheck,
-  useReadinessCheck,
-  createQueryHook,
-  createMutationHook,
-} from './hooks'
+export { createQueryHook, createMutationHook } from './hooks'
 export type { paths } from './schema'
