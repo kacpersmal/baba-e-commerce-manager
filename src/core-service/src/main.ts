3cc843ee7d3bd596ebf07288db72ebb11701741f
Kacper S <kacpersmal@gmail.com>
2025-10-25 16:31:55 +0200

    setup base env

--- src/core-service/src/main.ts
+++ src/core-service/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Enable CORS only in development
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
+  }
+  
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
