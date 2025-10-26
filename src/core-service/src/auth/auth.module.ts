f0c2cf8c7dbfaa08391bf4f24c6d94399ca88344
Kacper S <kacpersmal@gmail.com>
2025-10-26 09:31:16 +0100

    init base auth services

--- src/core-service/src/auth/auth.module.ts
+++ src/core-service/src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TokenService } from './token.service';
+import { HashService } from './hash.service';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService, TokenService, HashService]
 })
 export class AuthModule {}
