ade07093230465b2b73ba9e775b031c74f156c00
Kacper S <kacpersmal@gmail.com>
2025-10-26 09:19:25 +0100

    init auth module

--- src/core-service/src/app.module.ts
+++ src/core-service/src/app.module.ts
@@ -3,8 +3,9 @@ import { AppConfigModule } from '@shared/config';
 import { PrismaModule } from '@shared/prisma';
 import { RedisModule } from '@shared/redis';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AppConfigModule, PrismaModule, RedisModule, HealthModule],
+  imports: [AppConfigModule, PrismaModule, RedisModule, HealthModule, AuthModule],
 })
 export class AppModule {}
