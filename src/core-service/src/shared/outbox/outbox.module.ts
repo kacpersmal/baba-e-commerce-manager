ccb3726baa153f9b26689cbd01814d7f53f7e8d6
Kacper S <kacpersmal@gmail.com>
2025-10-26 07:59:32 +0100

    outbox example

--- src/core-service/src/shared/outbox/outbox.module.ts
+++ src/core-service/src/shared/outbox/outbox.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OutboxService } from './outbox.service';
 import { OutboxPublisherService } from './outbox-publisher.service';
+import { OutboxController } from './outbox.controller';
 import { ScheduleModule } from '@nestjs/schedule';
 import { EventEmitter } from 'stream';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [ScheduleModule.forRoot(), EventEmitterModule.forRoot()],
+  controllers: [OutboxController],
   providers: [OutboxService, OutboxPublisherService],
   exports: [OutboxService],
 })
